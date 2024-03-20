a47eed716e89267e57a3612af9c67acdbb6b6b5f
adrianarmm <areyemor@myuax.com>
2024-03-20 19:51:31 +0100

    27

--- src/main/java/EditorDocumento.java
+++ src/main/java/EditorDocumento.java
@@ -1,2 +1,14 @@
-public class EditorDocumento {
+import javax.swing.*;
+
+public class EditorDocumento extends JFrame {
+    private JTextArea areaDeTexto;
+
+    public EditorDocumento() {
+        setTitle("Nuevo Documento");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        areaDeTexto = new JTextArea();
+        add(new JScrollPane(areaDeTexto));
+    }
 }
