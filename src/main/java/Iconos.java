e31042098933af1529e1149e19b40b65c3f1e2e3
adrianarmm <areyemor@myuax.com>
2024-03-22 19:39:28 +0100

    47

--- src/main/java/Iconos.java
+++ src/main/java/Iconos.java
@@ -3,9 +3,9 @@ import javax.swing.*;
 public class Iconos {
     public static void main(String[] args) {
 
-        ImageIcon iconoEditor = new ImageIcon("documentos/desarrollo-web.png");
-        ImageIcon iconoDibujo = new ImageIcon("documentos/computadora.png");
-        ImageIcon iconoEmail = new ImageIcon("documentos/email.png");
+        ImageIcon iconoEditor = new ImageIcon("/Users/adrianareyesmorera/Documents/desarrollo-web.png");
+        ImageIcon iconoDibujo = new ImageIcon("/Users/adrianareyesmorera/Documents/computadora.png");
+        ImageIcon iconoEmail = new ImageIcon("/Users/adrianareyesmorera/Documents/email.png");
 
         JOptionPane.showMessageDialog(null, "Icono de editor", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoEditor);
         JOptionPane.showMessageDialog(null, "Icono de dibujo", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoDibujo);
