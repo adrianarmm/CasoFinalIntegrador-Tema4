8d8c7e5823e6d89181d87c8a8a13714fcf30f969
adrianarmm <areyemor@myuax.com>
2024-03-22 20:01:57 +0100

    54

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class MAIN extends JFrame {
     private JMenuBar menuBar;
     private JMenu menuArchivo, menuHerramientas;
-    private JMenuItem menuItemEditor, menuItemDibujo, menuItemValidadorEmail;
+    private JMenuItem menuItemEditor, menuItemDibujo, menuItemValidadorEmail, menuItemAgenda;
 
     public MAIN() {
         inicializarUI();
@@ -17,43 +17,40 @@ public class MAIN extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         menuBar = new JMenuBar();
+
         menuArchivo = new JMenu("Archivo");
         menuHerramientas = new JMenu("Herramientas");
 
-        // Cargar y escalar iconos
-        ImageIcon iconoEditor = new ImageIcon(escalarImagen(new ImageIcon("/Users/adrianareyesmorera/Documents/desarrollo-web.png"), 20, 20));
-        ImageIcon iconoDibujo = new ImageIcon(escalarImagen(new ImageIcon("/Users/adrianareyesmorera/Documents/computadora.png"), 20, 20));
-        ImageIcon iconoEmail = new ImageIcon(escalarImagen(new ImageIcon("/Users/adrianareyesmorera/Documents/email.png"), 20, 20));
-
-        menuItemEditor = new JMenuItem("Editor de Texto", iconoEditor);
-        menuItemDibujo = new JMenuItem("Herramienta de Dibujo", iconoDibujo);
-        menuItemValidadorEmail = new JMenuItem("Validador de Email", iconoEmail);
+        menuItemEditor = new JMenuItem("Editor de Texto");
+        menuItemDibujo = new JMenuItem("Herramienta de Dibujo");
+        menuItemValidadorEmail = new JMenuItem("Validador de Email");
+        menuItemAgenda = new JMenuItem("Agenda de Contactos"); // Nuevo ítem de menú para la agenda
 
         menuArchivo.add(menuItemEditor);
         menuHerramientas.add(menuItemDibujo);
         menuHerramientas.add(menuItemValidadorEmail);
+        menuHerramientas.add(menuItemAgenda); // Añadir la agenda al menú Herramientas
 
         menuBar.add(menuArchivo);
         menuBar.add(menuHerramientas);
 
         setJMenuBar(menuBar);
 
-        // Configurar acción para abrir el editor de texto
         menuItemEditor.addActionListener(e -> {
-            EditorDocumento editor = new EditorDocumento(); // Crear una instancia del editor
-            editor.setVisible(true); // Hacer visible el editor
+            EditorDocumento editor = new EditorDocumento();
+            editor.setVisible(true);
         });
 
         menuItemDibujo.addActionListener(e -> JOptionPane.showMessageDialog(this, "Funcionalidad de dibujo aún no implementada.", "Información", JOptionPane.INFORMATION_MESSAGE));
         menuItemValidadorEmail.addActionListener(e -> JOptionPane.showMessageDialog(this, "Funcionalidad de validación de email aún no implementada.", "Información", JOptionPane.INFORMATION_MESSAGE));
-    }
 
-    private Image escalarImagen(ImageIcon iconoOriginal, int anchoDeseado, int altoDeseado) {
-        return iconoOriginal.getImage().getScaledInstance(anchoDeseado, altoDeseado, Image.SCALE_SMOOTH);
+        menuItemAgenda.addActionListener(e -> {
+            AgendaDeContactos agenda = new AgendaDeContactos(); // Instanciar la agenda de contactos
+            agenda.setVisible(true); // Hacer visible la agenda
+        });
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new MAIN().setVisible(true));
     }
 }
-
