f0d366a26c89153f910f2547a9cbe4050ff5452e
adrianarmm <areyemor@myuax.com>
2024-03-22 22:34:45 +0100

    75

--- src/main/java/Iconos.java
+++ src/main/java/Iconos.java
@@ -23,7 +23,27 @@ public class Iconos {
         ImageIcon iconoAgendaOriginal = new ImageIcon("/Users/adrianareyesmorera/Downloads/agenda-de-contactos.png");
         ImageIcon iconoAgendaEscalado = escalarImagen(iconoAgendaOriginal, 50, 50); // Ajusta el tamaño deseado aquí
         JOptionPane.showMessageDialog(null, "Icono de agenda", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoAgendaEscalado);
-    }
+
+        // Escalar y mostrar el icono de busqueda de palabras
+        ImageIcon iconoBusquedaPalabrasOriginal = new ImageIcon("/Users/adrianareyesmorera/Downloads/busqueda-por-palabras.png");
+        ImageIcon iconoBusquedaPalabrasEscalado = escalarImagen(iconoBusquedaPalabrasOriginal, 50, 50); // Ajusta el tamaño deseado aquí
+        JOptionPane.showMessageDialog(null, "Icono de búsqueda de palabras", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoBusquedaPalabrasEscalado);
+
+        // Escalar y mostrar el icono de documento con barra de desplazamiento
+        ImageIcon iconoDocConBarraDesplazamientoOriginal = new ImageIcon("/Users/adrianareyesmorera/Downloads/desplacese-hacia-abajo.png");
+        ImageIcon iconoDocConBarraDesplazamientoEscalado = escalarImagen(iconoDocConBarraDesplazamientoOriginal, 50, 50); // Ajusta el tamaño deseado aquí
+        JOptionPane.showMessageDialog(null, "Icono de documento con barra de desplazamiento", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoDocConBarraDesplazamientoEscalado);
+
+        // Escalar y mostrar el icono de editor de texto con listado
+        ImageIcon iconoTextEditorConListingOriginal = new ImageIcon("/Users/adrianareyesmorera/Downloads/lista-de-texto.png");
+        ImageIcon iconoTextEditorConListingEscalado = escalarImagen(iconoTextEditorConListingOriginal, 50, 50); // Ajusta el tamaño deseado aquí
+        JOptionPane.showMessageDialog(null, "Icono de editor de texto con listado", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoTextEditorConListingEscalado);
+
+        // Escalar y mostrar el icono de seguimiento de raton
+        ImageIcon iconoSeguimientoRatonOriginal = new ImageIcon("/Users/adrianareyesmorera/Downloads/bola-de-seguimiento.png");
+        ImageIcon iconoSeguimientoRatonEscalado = escalarImagen(iconoSeguimientoRatonOriginal, 50, 50); // Ajusta el tamaño deseado aquí
+        JOptionPane.showMessageDialog(null, "Icono de seguimiento de ratón", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoSeguimientoRatonEscalado);
+ }
 
     // Método auxiliar para escalar la imagen
     private static ImageIcon escalarImagen(ImageIcon iconoOriginal, int anchoDeseado, int altoDeseado) {
