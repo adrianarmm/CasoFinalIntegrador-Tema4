3154a4b8cf5b6c5d495ea2644e8b57943c31590f
adrianarmm <areyemor@myuax.com>
2024-03-22 19:28:33 +0100

    44

--- src/main/java/Iconos.java
+++ src/main/java/Iconos.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+
 public class Iconos {
-}
+    public static void main(String[] args) {
+
+        ImageIcon iconoEditor = new ImageIcon("path/to/editor_icon.png");
+        ImageIcon iconoDibujo = new ImageIcon("path/to/drawing_icon.png");
+        ImageIcon iconoEmail = new ImageIcon("path/to/email_icon.png");
