92d58ad59069e3ce2d9c5c0dee78d949a83fc758
adrianarmm <areyemor@myuax.com>
2024-03-22 19:29:04 +0100

    45

--- src/main/java/Iconos.java
+++ src/main/java/Iconos.java
@@ -6,3 +6,9 @@ public class Iconos {
         ImageIcon iconoEditor = new ImageIcon("path/to/editor_icon.png");
         ImageIcon iconoDibujo = new ImageIcon("path/to/drawing_icon.png");
         ImageIcon iconoEmail = new ImageIcon("path/to/email_icon.png");
+
+        JOptionPane.showMessageDialog(null, "Icono de editor", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoEditor);
+        JOptionPane.showMessageDialog(null, "Icono de dibujo", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoDibujo);
+        JOptionPane.showMessageDialog(null, "Icono de email", "Icono", JOptionPane.INFORMATION_MESSAGE, iconoEmail);
+    }
+}
