372a73075c2bfe327df03e518a065847d5eb6974
adrianarmm <areyemor@myuax.com>
2024-03-22 19:59:45 +0100

    53

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -57,16 +57,3 @@ public class MAIN extends JFrame {
     }
 }
 
-class EditorDocumento extends JFrame {
-    private JTextArea areaDeTexto;
-
-    public EditorDocumento() {
-        setTitle("Nuevo Documento");
-        setSize(300, 200);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        areaDeTexto = new JTextArea();
-        add(new JScrollPane(areaDeTexto));
-    }
-}
-
