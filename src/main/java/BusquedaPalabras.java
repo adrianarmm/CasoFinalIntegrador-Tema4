0b90a56a3c0866dd9636e81d29613da3b5592782
adrianarmm <areyemor@myuax.com>
2024-03-24 19:48:30 +0100

    90

--- src/main/java/BusquedaPalabras.java
+++ src/main/java/BusquedaPalabras.java
@@ -5,7 +5,7 @@ public class BusquedaPalabras {
     /**
      * Busca una palabra específica en un documento y cuenta cuántas veces aparece.
      *
-     * @param   /Users/adrianareyesmorera/Documents/MENUSTARTASDEQUESO.txt El archivo en el que buscar la palabra.
+     * @param   /Users/adrianareyesmorera/Documents/TARTAS-QUESO.txt El archivo en el que buscar la palabra.
      * @param searchWord La palabra a buscar.
      * @return El número de veces que la palabra aparece en el documento.
      * @throws IOException Si ocurre un error al leer el archivo.
@@ -20,7 +20,7 @@ public class BusquedaPalabras {
                 // Convierte cada línea a minúsculas para hacer la búsqueda insensible a mayúsculas/minúsculas.
                 line = line.toLowerCase();
                 // Divide la línea en palabras usando espacios y puntuación como delimitadores.
-                String[] words = line.split("Tarta"); // "\\W+" es una expresión regular que representa uno o más caracteres no alfabéticos.
+                String[] words = line.split("W+"); // "\\W+" es una expresión regular que representa uno o más caracteres no alfabéticos.
                 for (String word : words) {
                     if (word.equals(searchWord)) {
                         count++;
@@ -33,7 +33,7 @@ public class BusquedaPalabras {
     }
 
     public static void main(String[] args) {
-        File file = new File("/Users/adrianareyesmorera/Documents/TARTASQUESO.txt");
+        File file = new File("/Users/adrianareyesmorera/Documents/TARTAS-QUESO.txt"); // La ruta del archivo en el que buscar la palabra.
         String searchWord = "Tarta"; // La palabra que quieres buscar en el documento.
 
         try {
